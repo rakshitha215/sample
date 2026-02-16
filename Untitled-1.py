b64c1d506618da5a934dbde17a8164911185e0ec
Rakshitha-B-A <rakshithaba152@gmail.com>
2026-02-16 14:03:11 +0530

    removed commented lines

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,18 +1,18 @@
-# #Find the largest number numbers = [45, 99, 3, 20, 60]
-# list1=[45, 99, 3, 20, 60]
-# print(max(list1))
-# #Duplicate_numbers=[1,1,2,3,4,4,5,5,6,6,6]
-# Duplicate_numbers=[1,1,2,3,4,4,5,5,6,6,6]
-# repeated=[]
-# for i in Duplicate_numbers:
-#     if Duplicate_numbers.count(i)>1:
-#         if i not in repeated:
-#             repeated.append(i) 
-# print("Duplicate numbers in list are",repeated)
-# # Zero in last nums = [0, 1, 0, 3, 0, 12]
-# temperature_in_fah= int(input("Enter the Temperature value in Fahrenite"))
-# celcius=(temperature_in_fah-32)*(5/9)
-# print("Temperature in celcius is ",round(celcius))
+#Find the largest number numbers = [45, 99, 3, 20, 60]
+list1=[45, 99, 3, 20, 60]
+print(max(list1))
+#Duplicate_numbers=[1,1,2,3,4,4,5,5,6,6,6]
+Duplicate_numbers=[1,1,2,3,4,4,5,5,6,6,6]
+repeated=[]
+for i in Duplicate_numbers:
+    if Duplicate_numbers.count(i)>1:
+        if i not in repeated:
+            repeated.append(i) 
+print("Duplicate numbers in list are",repeated)
+# Zero in last nums = [0, 1, 0, 3, 0, 12]
+temperature_in_fah= int(input("Enter the Temperature value in Fahrenite"))
+celcius=(temperature_in_fah-32)*(5/9)
+print("Temperature in celcius is ",round(celcius))
 Duplicate_numbers = [1,1,2,3,4,4,5,5,6,6,6]
 
 seen = set()
