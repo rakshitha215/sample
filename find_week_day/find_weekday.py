cd4714b053cef2ecd861d94ad925a2c74ea2d03e
Rakshitha215 <pachhallerrakshitha@email.com>
2026-02-19 15:34:19 +0530

    updated

--- find_week_day/find_weekday.py
+++ find_week_day/find_weekday.py
@@ -2,5 +2,3 @@ import calendar
 month, day, year = map(int, input().split())
 day_number = calendar.weekday(year, month, day)
 print(calendar.day_name[day_number]                 )
-print(calendar.month(2024, 2))
-print(calendar.calendar(2024))
